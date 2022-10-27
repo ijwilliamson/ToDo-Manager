5a210844b5a254ce3117ea059b076a706ad89787
Ian Williamson <ian@4of.uk>
2022-10-27 07:55:30 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
+import ToDoItem from './ToDo-Item';
+import ToDoCreator from './ToDoCreator';
+import toDos from './startingItems';
+
+let firstRun = true;
+
 
 function App() {
+
+  const [tItems, tItemsUpdate] = useState([]);
+  
+
+  if(firstRun) {
+    
+    tItemsUpdate([...toDos]);
+    firstRun = false;
+  }
+  
+
+    const JSXArray = toDos.map((item, id) => {
+      return (
+        <ToDoItem key={id} title={item.title}
+        notes={item.notes} created={item.created}
+        doByDate={item.doByDate} complete={item.complete}
+        completedOn={item.completedOn} category={item.category}
+        urgent={item.urgent} ></ToDoItem>
+      )
+    });
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <to-Do>
+      <toDo-header><ToDoCreator/></toDo-header>
+      <toDo-main>
+        <toDo-Col>
+          <header>To Do</header>
+          <toDo-content>
+            {JSXArray}
+          </toDo-content>
+          <footer></footer>
+        </toDo-Col>
+        <toDo-Col>
+        <header>Completed</header>
+          <toDo-content>
+
+          </toDo-content>
+          
+        </toDo-Col>
+        <toDo-DayGroup>
+          <toDo-Day>
+            <header>URGENT</header>
+          </toDo-Day>
+          <toDo-Day>
+          <header>PARKED</header>
+          </toDo-Day>
+          <toDo-Day>
+            <header>BIN</header>
+          </toDo-Day>
+          </toDo-DayGroup>
+      </toDo-main>
+      <toDo-footer></toDo-footer>
+      
+    </to-Do>
   );
 }
+
+
+
 
 export default App;
---
+++ src/startingItems.js
@@ -0,0 +1,46 @@
+
+
+const toDos =[
+    {id: 1,
+    title: "Finish Calculator",
+    notes: "Dah Dah Dah",
+    created: new Date('2022-10-26T12:23:00'),
+    doByDate: new Date('2022-10-26T12:23:00'),
+    complete: false,
+    completedOn: null,
+    category: "CODING",
+    urgent: false},
+    
+    {id: 2,
+    title: "Complete weekly One File",
+    notes: "Dah Dah Dah",
+    created: new Date('2022-10-26T12:23:00'),
+    doByDate: new Date('2022-10-27T12:23:00'),
+    complete: false,
+    completedOn: null,
+    category: "ADMIN",
+    urgent: false},
+    
+    {id: 3,
+    title: "Add Calculator to Git Pages",
+    notes: "Dah Dah Dah",
+    created: new Date('2022-10-26T12:23:00'),
+    doByDate: new Date('2022-10-28T12:23:00'),
+    complete: false,
+    completedOn: null,
+    category: "DEVELOPMENT",
+    urgent: false},
+    
+    {id: 4,
+    title: "Complete To Do List manager",
+    notes: "Dah Dah Dah",
+    created: new Date('2022-10-26T12:23:00'),
+    doByDate: new Date('2022-10-28T12:23:00'),
+    complete: false,
+    completedOn: null,
+    category: "CODING",
+    urgent: false}
+        
+]
+
+export default toDos;
